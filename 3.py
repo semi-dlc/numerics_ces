8e543f5ba1c98ea96f0899da04e19daef8c2d2b2
warisa-r <81345089+warisa-r@users.noreply.github.com>
2023-05-30 00:05:09 +0200

    3.py
    Aufgabe b added.
    
    Please check c.

--- 3.py
+++ 3.py
@@ -16,6 +16,8 @@ def lr_ohne(A):
             R[j] = R[j] - l*R[i]
     return L, R
 
+
+
 def lr_mit (A):
     n = len(A)
     L = np.eye(n, dtype=float)
@@ -58,7 +60,7 @@ def linsolve_ohne(A, b):
     #backward substitution
     for i in reversed(range(n)):
         for j in range (n-i-1):
-            #print(f"i: {i}, j: {j} ", end='') -> uncomment this line to understand how things workss
+            #print(f"i: {i}, j: {j} ", end='') -> uncomment this line to understand how things works
             x_d[i] -= R[i][n-j-1] * x[n-j-1]
         
         if(R[i][i] == 0):
@@ -67,39 +69,72 @@ def linsolve_ohne(A, b):
         
         x[i] = x_d[i] / R[i][i]
     return x
+
+
 
 def linsolve_mit(A, b):
     n = len(A)
     x_d = np.zeros(n, dtype=float)
     L, R, P =  lr_mit(A)
-    b = np.dot(b, P)
+    b = np.dot(P,b)
     #forward substitution
     for i in range (n):
         for j in range (i):
             b[i] -= L[i][j] * x_d[j]
         x_d[i] = b[i]
-
+    
+    print (x_d)
     x = np.zeros(n, dtype=float)
     #backward substitution
-    for i in reversed(range(n)):
-        for j in range (n-i-1):
-            #print(f"i: {i}, j: {j} ", end='') -> uncomment this line to understand how things workss
-            x_d[i] -= R[i][n-j-1] * x[n-j-1]
-        
-        if(R[i][i] == 0):
-            print ("Cannot solve this LGS")
+    for i in range(n-1, -1, -1):
+        x[i] = x_d[i]
+        for j in range(n-1, i, -1):
+            #print(f"i: {i}, j: {j} ", end='') -> uncomment this line to understand how things works
+            x[i] -= R[i][j] * x[j]
+    
+        if R[i][i] == 0:
+            print("Cannot solve this LGS")
             return
         
-        x[i] = x_d[i] / R[i][i]
+        x[i] /= R[i][i]
+
     return x
 
 def main():
+
+    ## a. ##
     A = np.array([[1, 1, 3], [1, 2, 2], [2, 1, 5]])
     b = [2, 1, 1]
+    x_1 = linsolve_mit(A,b)
+    x_2 = linsolve_ohne(A,b)
+    
+    print (x_1)
+    print (x_2)
+    
+    ## b. ##
 
-    x = linsolve_mit(A, b)
+    A = np.array([[0, 1, 0, -1],
+              [1, 2, 2, 1],
+              [1, 1, 1, 1],
+              [2, 1, -1, 2]])
 
-    print (x)
+    b = np.array([2, -1, -2, -11])
 
+    #x1 = linsolve_ohne (A,b) #Cannot solve this LGS
+    x2 = linsolve_mit (A,b)
+    #print (x1)
+    print (x2)
 
+    ## c. ##
+
+    np.random.seed(42)
+    epsilon = np.random.normal(0, 1)
+
+    array_of_matrices = []
+    for n in range(2, 7):
+        matrix = np.zeros((n, n))
+        for i in range (n):
+            for j in range (n):
+                matrix [i][n-j-1] = pow (3, -abs(i-j)) + pow (2, -(i-1+j-1)) + pow (10,-10) * epsilon
+        array_of_matrices.append(matrix)
 main()
