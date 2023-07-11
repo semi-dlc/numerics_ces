6e40a4aba8c4ca65a0290b42ebd3a20d2975dcb2
semi f <71894595+semifake-612@users.noreply.github.com>
2023-07-12 00:21:43 +0200

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,19 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import random 
+import math
 
 def f1(x):
     #fx1 = x[0]**2 + x[1]**2 - 1
     #fx2 = x[0]*x[1] - 1/4
     return np.array([x[0]**2 + x[1]**2 - 1, x[0]*x[1] - 1/4])
 #roots are sign(sqrt(0.5+sqrt(3)/4))
-f1_root1 = [np.sqrt(0.5 + np.sqrt(3) / 4), 1/(4 * np.sqrt(0.5 + np.sqrt(3) / 4))]
-f1_root2 = [np.sqrt(0.5 + np.sqrt(3) / 4) * -1, -1/(4 * np.sqrt(0.5 + np.sqrt(3) / 4))]
-f1_root3 = [1/(4 * np.sqrt(0.5 + np.sqrt(3) / 4)), np.sqrt(0.5 + np.sqrt(3) / 4)]
-f1_root4 = [-1/(4 * np.sqrt(0.5 + np.sqrt(3) / 4)), -1 * np.sqrt(0.5 + np.sqrt(3) / 4)] 
-print(f1_root1)
-print(f1_root2)
-print(f1_root3)
-print(f1_root4)
+
 
 
 def Df1 (x):
@@ -43,7 +38,7 @@ def Df3(x):
   #  Dx1 = -1
 #    Dy0 = -1
 #    Dy1 = 2*x[1]
-    return np.array([[NaN, -1],[-1, 2*x[1]]])
+    return np.array([[0.5 * np.sin(np.pi * x[0] / 2) + 0.25 * x[0] * np.pi * np.cos(np.pi * x[0] / 2), -1],[-1, 2*x[1]]])
 
 
 def f4 (x):
@@ -76,7 +71,7 @@ def Df5(x, j_d):
     df_da = j_d * z**(j_d-1)
     df_db = j_d * z**(j_d-1) * 1j
     
-    return np.array([[df_da.real, df_da.imag], [df_db.real, df_db.imag]])
+    return np.array([[df_da.real, df_db.real], [df_da.imag, df_db.imag]])
 
 ## TBD: partial derivative of gj
     #We need to first analyze how this works.
@@ -103,7 +98,7 @@ def error_C(x, zj):
 ## in place how? One possiblity is to save the old value in temp but u still need the origianl x to input it in Df tho
 
 
-def newton (x0, f, Df, tol, itmax, x = []):
+def newton_old_insecure (x0, f, Df, tol, itmax, x = []):
     x = []
     x.append(x0)
     x_new  = x[0] - np.linalg.inv(Df(x[0])).dot(f(x[0]))
@@ -115,17 +110,66 @@ def newton (x0, f, Df, tol, itmax, x = []):
         itmax += 1
     return x[itmax], itmax
 
+def newton (x0, f, Df, tol, itmax, x = []): #this one is used
+    Singular_Count = 0 #if too many singular matrices, return error
+    x = []
+    x.append(x0)
+    print(f(x0))
+    print(Df(x0))
+    try:
+        x_new  = x[0] - np.linalg.inv(Df(x[0])).dot(f(x[0]))
+    except np.linalg.LinAlgError: # be x a vector of real numbers
+        epsilonJacobian = 0.01
+
+        print("Jacobian is singular. x gets a slight offset")
+        for i in range(len(x[0])):
+            x[0][i] = x[0][i] + i * 0.01
+            print(x[0][i])
+        print(x[0])
+        x_new  = x[0] - np.linalg.inv(Df(x[0])).dot(f(x[0]))
+    x.append(x_new)
+    itmax = 0
+    while error(x[itmax+1], x[itmax]) > tol * np.linalg.norm(x[itmax], ord=2):
+        try:
+            x_new  = x[itmax+1] - np.linalg.inv(Df(x[itmax+1])).dot(f(x[itmax+1]))
+        except np.linalg.LinAlgError: # be x a vector of real numbers
+            Singular_Count += 1
+            epsilonJacobian = 0.01
+            print("Jacobian is singular. x gets an offset")
+            if Singular_Count > 7:
+                print("Newton iteration might be stuck in a loop. The Jacobian is regularly singular")
+                return x, itmax
+            for i in range(len(x[0])):
+                x[itmax + 1][i] = x[itmax + 1][i] + i
+                print(x[itmax + 1][i])
+            print(x[itmax + 1])
+            x_new  = x[itmax + 1] - np.linalg.inv(Df(x[itmax + 1])).dot(f(x[itmax + 1]))
+        x.append(x_new)
+        itmax += 1
+    return x, itmax
+
 
 def newton_C (x0, j, f, Df, tol, itmax, x = []):
     x = []
     x.append(x0)
-    x_new  = x[0] - np.linalg.inv(Df(x[0], j)).dot(f(x[0], j))
+    try:
+        x_new  = x[0] - np.linalg.inv(Df(x[0], j)).dot(f(x[0], j))
+    except :
+        x_new = [math.nan, math.nan]
+        x.append(x_new)
+        return x, itmax
     x.append(x_new)
     itmax = 0
 
     ## idea ## find out each time which element of zj is closest to the current x[itmax] using min {}
     while error_C(x[itmax], zj(j)) >= tol: ## to be worked on. The condition here is different and has sth to do with zj
-        x_new  = x[itmax+1] - np.linalg.inv(Df(x[itmax+1], j)).dot(f(x[itmax+1],j))
+        try:
+            x_new  = x[itmax+1] - np.linalg.inv(Df(x[itmax+1], j)).dot(f(x[itmax+1],j))
+        except :
+            x_new = [math.nan, math.nan]
+            x.append(x_new)
+            itmax += 1
+            return x, itmax
         x.append(x_new)
         itmax += 1
     
@@ -145,27 +189,66 @@ def newton_1D(x0, f, Df, tol, it_max, x=[]):
     
     return x[itmax], itmax
 
+## to do for fun.. (?) do a function that is a map -> so Df can be a function applicable to all functions from f1-f3
+## right now newton works fine
+
+##############fun by dino
+
+def log_transform(l):
+    return (2**(l+1)) / (2**l + 1) - 1
+def simple_transform(l):  #faster to compute
+    return l / (abs(l) + 1)
+def plot_R2(x_it, fx): # be x_it domain, and fx image
+    stepbystep = False
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection = '3d')
+    for i in range(len(x_it)):
+        #coord = [x_it[i][0], x_it[i][1], fx[i][0]]
+    #print(coord)
+     #   print(fx[i][1])
+     #   print("Color (normalized)")
+        color__ = simple_transform(fx[i][1]) 
+      #  print(color__)
+        if stepbystep:
+            while (input() == None): #step by step iteration
+                pass
+        if color__ >= 0:
+            ax.scatter(x_it[i][0], x_it[i][1], fx[i][0], c = [(1-color__, 1, 1)], edgecolors = (0,0,0))
+        else: 
+            ax.scatter(x_it[i][0], x_it[i][1], fx[i][0], c = [(1, 1-abs(color__), 1)], edgecolors= (0, 0, 0))
+        fig.show() 
+    fig.show()
+    s = None
+    while (input("Press to continue") == None):
+        pass
+
+
+def wrapper(fn, Dfn, starting_point = np.array([1,1])):
+    itmax = 2
+    x1, itmax = newton(starting_point, fn, Dfn, 0.0000000000001, itmax)
+    print (x1)
+    print (itmax)
+    fx1 = []
+    for i in x1:
+        fx1.append(f1(i))
+    plot_R2(x1, fx1)    	    
+
+
+
 
 def main():
     ## partially a.) ##
     ## TBD !! ##
+    '''
     x1_0 = np.array ([1,2])
-    itmax = 2
-    print("Root starting at ") 
-    print(x1_0)
-    x1, itmax = newton(x1_0, f1, Df1, 0.000001, itmax)
-    print (x1)
-    print("Iterations")
-    print (itmax)
+    wrapper(f1, Df1, x1_0)
 
-    x2_0 = np.array([1, -2])
-    itmax = 2
-    print("Root starting at ") 
-    print(x2_0)
-    x2, itmax = newton(x2_0, f2, Df2, 0.000001, itmax)
-    print (x2)
-    print("Iterations")
-    print (itmax)
+    x2_0 = np.array([1, -3])
+    wrapper(f2, Df2, x2_0)
+    
+    x3_0 = np.array([1, -2])
+    wrapper(f3, Df3, x3_0)
 
 
     ## b.) ##
@@ -196,13 +279,6 @@ def main():
     plt.show()
     
     '''
-    x5_0 = np.array([1, 2]) 
-    itmax5 = 0
-    j = 3
-    x5, itmax5 = newton_C(x5_0, j, f5, Df5, 0.0000000000001, itmax5)
-    print (x5)
-    print (zj(3)[2]) ##Same :D Or am I just delusional
-    '''
     j = 5
 
     min_re = -1
@@ -210,23 +286,30 @@ def main():
     min_im = -1
     max_im = 1
 
-    num_points = 100
+    num_points = 5
 
 
     # Create the grid of points
     x = np.linspace(min_re, max_re, num_points)
     y = np.linspace(min_im, max_im, num_points)
     X, Y = np.meshgrid(x, y)
-
+    print("X")
+    print(X)
+    print("Y")
+    print(Y)
     x5_0 = np.vstack([X.flatten(), Y.flatten()]).T
     x5 = np.zeros_like(x5_0)
     it5 = np.zeros_like(x5_0)
-
+    fractal_map = np.zeros((num_points, num_points))
+    roots = zj(j)
     for i in range (0, num_points):
-        x5[i], it5[i] = newton_C(x5_0[i], j, f5, Df5, 0.00000000001, it5[i])
-        print (x5[i])
+        for k in range (0, num_points):
+            x5[i], it5[i] = newton_C(x5_0[i], j, f5, Df5, 0.000000001, it5[i])
+            print (x5[i])         
+            for l in roots:
+                if error(x5[i], )
+    print (zj(j))
 
-    print (zj(5))
     
 main()
 
